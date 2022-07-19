d19cb571152cda742f490cc4dd6f00a1e5ecd8eb
sik9252 <sik9252@naver.com>
2022-07-19 11:38:50 +0900

    feat: Seo.js 컴포넌트를 통한 Head(Tab title) 설정

---
+++ components/Seo.js
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+
+function Seo({title}) {
+  return (
+    <div>
+      <Head>
+        <title>{title} | Next Movies</title>
+      </Head>
+    </div>
+  )
+}
+
+export default Seo
