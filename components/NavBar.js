d226379920fe3287b3dfeff40ada031e50190ace
sik9252 <sik9252@naver.com>
2022-07-19 13:59:40 +0900

    style: NavBar 스타일 추가

--- components/NavBar.js
+++ components/NavBar.js
@@ -30,28 +30,42 @@ import { useRouter } from "next/router";
 
 export default function NavBar() {
   const router = useRouter();
-
   return (
     <nav>
-      <Link href="/">
-        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
-      </Link>
-      <Link href="/about">
-        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
-      </Link>
-
+      <img src="/vercel.svg" />
+      <div>
+        <Link href="/">
+          <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+        </Link>
+        <Link href="/about">
+          <a className={router.pathname === "/about" ? "active" : ""}>About</a>
+        </Link>
+      </div>
       <style jsx>{`
         nav {
-          background-color: #ff6437;
-          padding: 5px;
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.5) 0px 30px 70px -15px,
+            rgba(0, 0, 0, 0.3) 0px 30px 50px -20px;
         }
-        a {
-          margin: 0 5px;
-          font-size: 20px;
-          text-decoration: none;
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
         }
         .active {
-          color: white;
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
